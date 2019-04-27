87f35486e8c91e0365772df96281664386392139
douglaskoe <douglasblima01@egmail.com>
2019-04-27 09:49:04 -0300

    Aula 04

---
+++ HandsOn/Aula04/Aula04_01_simple_xml_carregando_xml.php
@@ -0,0 +1,20 @@
+<?php
+
+// Carregando um XML de um arquivo
+
+$objSimple = new SimpleXMLElement('curso.xml', null, true);
+
+echo '<pre>';
+
+print_r($objSimple);
+
+echo '<hr>';
+
+$retornoXML = file_get_contents('curso.xml');
+
+foreach($objSimple as $curso){
+    echo 'Id do Curso: '.$curso->id;
+    echo '<br>Nome do Curso: '.$curso->nome;
+    echo '<br>Categoria do Curso: '.$curso->categoria;
+    echo '<hr>';
+}
